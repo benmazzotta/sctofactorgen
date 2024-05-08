437be53036deb28fb8a70866310abe8cab8d124b
Ben Mazzotta <325448+benmazzotta@users.noreply.github.com>
2024-05-08 17:38:06 -0400

    patch conflict with haven
    Haven's validated_labelled checks that labels are unique. That is generally not true of SCTO labels.
    
    I also disabled some warnings using the globalVariables function from utils.

---
+++ R/haven_disable_uniqueness_check.R
@@ -0,0 +1,33 @@
+## Discussion: Labelled has a uniqueness requirement that prevents reusing label lists. The requirement is implemented on the function set_value_labels(). So, we have to overwrite that function.
+## Here is the rewritten function from {haven}
+
+#' Title
+#'
+#' @param x an object with a labels attribute
+#'
+#' @return the same object
+#' @export
+#'
+#' @examples NULL
+validate_labelled <- function(x) {
+  labels <- attr(x, "labels")
+  if (is.null(labels)) {
+    return(x)
+  }
+
+  if (is.null(names(labels))) {
+    cli_abort("{.arg labels} must have names.")
+  }
+  # if (any(duplicated(stats::na.omit(labels)))) {
+  # cli_abort("{.arg labels} must be unique.")
+  # }
+
+  x
+}
+
+
+# environment(newfn) <- asNamespace('packagename')
+# assignInNamespace("oldfn", newfn, ns = "packagename")
+## This is the idiom for overwriting a function from the other namespace.
+environment(validate_labelled) <- asNamespace("haven")
+assignInNamespace("validate_labelled", validate_labelled, "haven")
---
+++ R/visible_binding_warnings.R
@@ -0,0 +1,5 @@
+## Appease warnings about visible bindings
+#'@importFrom utils globalVariables
+utils::globalVariables(
+  c("auth", "type", "list_name", "value", "label", "name", "choices", "root", "colhead", "data", "formid")
+)
